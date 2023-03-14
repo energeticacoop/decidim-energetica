83bcf920d02eeddf3bbc0f72b4a5fa03e86367a8
Juan Guijarro <juan@energetica.coop>
2023-03-14 12:42:56 +0100

    Added iniciatives

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require_relative 'application'
 
+require 'wicked_pdf'
+require_relative 'application'
+
 # Initialize the Rails application.
 Rails.application.initialize!
